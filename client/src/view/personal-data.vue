7ad1210d2167eabdea4b8cdef5dd51a6929389c8
zeng <512439078@qq.com>
2020-03-18 00:15:10 +0800

    edit data

--- client/src/view/personal-data.vue
+++ client/src/view/personal-data.vue
@@ -10,65 +10,51 @@
   <div>
     <Card title="我的资料" style="width:60%;margin-bottom:30px;text-align:left">
       <div style="margin-left:10%;margin-bottom:20px">
-         <Avatar icon="ios-person" size="100" src="http://118.89.125.57/images/xx.jpg" />
-      <Upload action="http://118.89.125.57/blog/upload" style="margin:10px;">
-       <Button>点击上传</Button>
-      </Upload>
+        <div :key="update">
+          <Avatar icon="ios-person" size="100" :src="host + info.avatar" v-if="info.avatar" />
+          <Avatar icon="ios-person" size="100" v-else />
+        </div>
+        <Upload
+          action="http://118.89.125.57/blog/upload"
+          style="margin:10px;"
+          :on-success="onUpload"
+          :show-upload-list='false'
+        >
+          <Button>点击上传</Button>
+        </Upload>
       </div>
       <Form :model="formItem" :label-width="80">
-        <FormItem label="Input">
-          <Input placeholder="Enter something..." style="width:30%;"></Input>
+        <FormItem label="昵称：">
+          <Input style="width:30%;" v-model="info.nickName"></Input>
         </FormItem>
-        <FormItem label="Select">
-          <Select style="width:30%;">
-            <Option value="beijing">New York</Option>
-            <Option value="shanghai">London</Option>
-            <Option value="shenzhen">Sydney</Option>
-          </Select>
-        </FormItem>
-        <FormItem label="DatePicker">
-          <Row>
-            <Col span="11">
-              <DatePicker type="date" placeholder="Select date"></DatePicker>
-            </Col>
-            <Col span="2" style="text-align: center">-</Col>
-            <Col span="11">
-              <TimePicker type="time" placeholder="Select time"></TimePicker>
-            </Col>
-          </Row>
-        </FormItem>
-        <FormItem label="Radio">
-          <RadioGroup>
-            <Radio label="male">Male</Radio>
-            <Radio label="female">Female</Radio>
+        <FormItem label="性别：">
+          <RadioGroup v-model="info.sex">
+            <Radio label="男">男</Radio>
+            <Radio label="女">女</Radio>
           </RadioGroup>
         </FormItem>
-        <FormItem label="Checkbox">
-          <CheckboxGroup>
-            <Checkbox label="Eat"></Checkbox>
-            <Checkbox label="Sleep"></Checkbox>
-            <Checkbox label="Run"></Checkbox>
-            <Checkbox label="Movie"></Checkbox>
-          </CheckboxGroup>
+        <FormItem label="出生日期">
+          <DatePicker
+            type="date"
+            placeholder="日期"
+            style="width: 200px"
+            @on-change="onChange"
+            value="yyyy"
+          ></DatePicker>
         </FormItem>
-        <FormItem label="Switch">
-          <i-switch size="large">
-            <span slot="open">On</span>
-            <span slot="close">Off</span>
-          </i-switch>
-        </FormItem>
-        <FormItem label="Slider">
-          <Slider range></Slider>
+        <FormItem label="来自：">
+          <Input style="width:50%;" v-model="info.address"></Input>
         </FormItem>
         <FormItem label="Text">
           <Input
             type="textarea"
             :autosize="{minRows: 2,maxRows: 5}"
-            placeholder="Enter something..."
+            placeholder="写点什么名言吧"
+            v-model="info.sign"
           ></Input>
         </FormItem>
         <FormItem>
-          <Button type="primary">Submit</Button>
+          <Button type="primary" @click="save">确定</Button>
           <Button style="margin-left: 8px">Cancel</Button>
         </FormItem>
       </Form>
@@ -80,11 +66,53 @@
 export default {
   data() {
     return {
-      imageUrl: ""
+      imageUrl: "",
+      info: {
+        address: "",
+        age: "",
+        avatar: "",
+        nickName: "",
+        sex: "",
+        sign: ""
+      },
+      host: "http://118.89.125.57/images/",
+      update: new Date()
     };
   },
+  created() {
+    this.loadInfo();
+  },
   methods: {
-
+    async loadInfo() {
+      let res = await this.$ajax.post("/blog/myInfo");
+      this.info = res.data.info;
+      delete this.info["id"];
+      delete this.info["createTime"];
+      delete this.info["role_id"];
+    },
+    onChange(v) {
+      let date = v.split("-");
+      let born_date = new Date().getFullYear() - parseInt(date[0]);
+      this.info.age = born_date.toString();
+    },
+    onUpload(v) {
+      if (v.success) {
+        this.$Message.success(v.message);
+        this.info.avatar = v.data.uploadUrl;
+        this.update = new Date();
+        return;
+      }
+      this.$Message.error("error");
+    },
+    async save(){
+      console.log(this.info)
+      let res = await this.$ajax.post('/blog/editData');
+      if(res.success){
+        this.$Message.success(res.message)
+        this.loadInfo()
+        return
+      }
+    }
   }
 };
 </script>
