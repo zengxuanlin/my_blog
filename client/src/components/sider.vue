0fd537bd5786575c74ae3a1a0a7fb52ae02ff66c
zengxuanlin <512439078@qq.com>
2020-03-18 08:59:42 +0800

    xx

--- client/src/components/sider.vue
+++ client/src/components/sider.vue
@@ -2,7 +2,7 @@
   <div>
     <Sider hide-trigger :style="{background: '#fff'}">
       <div class="demo-avatar" style="text-align:center">
-        <Avatar icon="ios-person" size="80" src="http://118.89.125.57/images/xx.jpg" />\
+        <Avatar icon="ios-person" size="80" src="http://118.89.125.57/images/xx.jpg" />
         <div>1111</div>
         <div>1111</div>
       </div>
